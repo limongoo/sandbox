b4a9f37d71aad359d71eb5e66e8dff155acfac07
Johnny Luangphasy <johnny@jluangphasy.com>
2019-01-23 14:58:04 -0800

    Fix 4 spaces to 2 spaces

--- create-react-app/sample/src/index.js
+++ create-react-app/sample/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <h1 className="title" onClick={ () => alert('wow') }>
-        Hello World
-    </h1>
-, document.getElementById('root'));
+  <h1 className="title" onClick={ () => alert('wow') }>
+    Hello World
+  </h1>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- react/sample/index.js
+++ react/sample/index.js
@@ -1,21 +1,30 @@
 function App () {
-    return React.createElement(
-        'h1',
-        {className: "title",
-        onClick: () => {
-            alert("wow");
-        }},
-        React.createElement(Heading, { text: 'Hello World' })
+  return React.createElement(
+    'h1',
+    {
+      className: "title",
+      onClick: () => {
+        alert("wow");
+      }},
+    React.createElement(
+      Heading,
+      {
+        text: 'Hello World'
+      }
     )
+  )
 }
 
 
 function Heading (props) {
-    return React.createElement(
-        'h2',
-        null,
-        props.text,
-    )
+  return React.createElement(
+    'h2',
+    null,
+    props.text,
+  )
 }
 
-ReactDOM.render(React.createElement(App), root);
+ReactDOM.render(
+  React.createElement(App),
+  root
+)
